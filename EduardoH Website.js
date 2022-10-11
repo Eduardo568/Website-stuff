fc75031f8d67d5148b4ed4d678feca6c7414df70
Eduardo568 <eduardo.herndez568@gmail.com>
2022-10-11 02:23:00 -0700

    My Website

--- EduardoH Website.js
+++ EduardoH Website.js
@@ -2,32 +2,27 @@ let toggleNavStatus = false;
 
 let toggleNav = function() {
     let getSidebar = document.querySelector(".sidenav");
-    let getSidebarUl = document.querySelector(".sidenav ul");
-    let getSidebarLink = document.querySelectorAll(".sidenav a");
+    let getSidebarDiv = document.querySelector(".sidelink");
+    let getSidebarDiv2 = document.querySelector(".sidelink2");
+    let getSidebarDiv3 = document.querySelector(".sidelink3");
+    let getSidebarDiv4 = document.querySelector(".sidelink4");
 
     if (toggleNavStatus === false) {
-        getSidebarUl.style.visibility = "visible";
         getSidebar.style.width = "272px";
+        getSidebarDiv.style.visibility = "visible"
+        getSidebarDiv2.style.visibility = "visible"
+        getSidebarDiv3.style.visibility = "visible"
+        getSidebarDiv4.style.visibility = "visible"
 
-        let arrayLength = getSidebarLink.length;
-
-        for (let i = 0; i < arrayLength; i++) {
-            getSidebarLink[i].style.opacity = "1";
-        }
         toggleNavStatus = true;
     }
     else if (toggleNavStatus === true) {
         getSidebar.style.width = "50px";
-
-        let arrayLength = getSidebarLink.length;
-
-        for (let i = 0; i < arrayLength; i++) {
-            getSidebarLink[i].style.opacity = "0";
-
-        }
-        getSidebarUl.style.visibility = "hidden";
-
+        getSidebarDiv.style.visibility = "hidden"
         toggleNavStatus = false;
 
+        getSidebarDiv2.style.visibility = "hidden"
+        getSidebarDiv3.style.visibility = "hidden"
+        getSidebarDiv4.style.visibility = "hidden"
     }
 }
